abfc4adecf2c5d17df45bee76228776d70c72053
Daalhe20 <dah0008@alu.medac.es>
2026-05-06 13:01:39 +0200

    upload auxiliar 1.0

---
+++ Cliente.java
@@ -0,0 +1,35 @@
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private String direccion;
+
+    public Cliente(int id, String nombre, String apellido, String telefono, String email, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getTelefono() { return telefono; }
+    public String getEmail() { return email; }
+    public String getDireccion() { return direccion; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public void setEmail(String email) { this.email = email; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    @Override
+    public String toString() {
+        return id + " - " + nombre + " " + apellido;
+    }
+}
