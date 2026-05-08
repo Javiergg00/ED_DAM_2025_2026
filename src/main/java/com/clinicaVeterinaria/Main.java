ce8b658cf047ea95d4d6c03a953f48552b1ea056
Daalhe20 <dah0008@alu.medac.es>
2026-05-08 20:14:51 +0200

    FEAT-integracion de auxiliar en main

--- src/main/java/com/clinicaVeterinaria/Main.java
+++ src/main/java/com/clinicaVeterinaria/Main.java
@@ -1,4 +1,21 @@
 package com.clinicaVeterinaria;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        RecepcionistaController controller = new RecepcionistaController(primaryStage);
+        Scene scene = new Scene(controller.getView(), 900, 650);
+        primaryStage.setTitle("Clínica Veterinaria - Recepcionista");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
