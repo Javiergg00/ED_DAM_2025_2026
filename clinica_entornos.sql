1ae46a1b4a1fb0f7e06f1e261f8775ddc855d1c8
alexqsada3 <quesada2006pro@gmail.com>
2026-05-13 09:32:12 +0200

    Actualización BD
    Actualización de la base de datos con nuevas tablas como la de "veterinario", nuevas columnas interrelacionadas con la tabla "animales" mediante una clave foranea.
    
    Signed-off-by: alexqsada3 <quesada2006pro@gmail.com>

--- clinica_entornos.sql
+++ clinica_entornos.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 13-04-2026 a las 15:39:27
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-05-2026 a las 17:31:12
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `clinica_entornos`
+-- Base de datos: `clinica_entornos.sql`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,12 @@ CREATE TABLE `animales` (
   `nombre` varchar(50) NOT NULL,
   `estado` varchar(50) NOT NULL,
   `sintomas` varchar(50) NOT NULL,
-  `receta` varchar(50) NOT NULL
+  `receta` varchar(50) NOT NULL,
+  `raza` varchar(100) DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL,
+  `especie` varchar(50) DEFAULT NULL,
+  `id_veterinario` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -53,10 +58,21 @@ CREATE TABLE `perfiles` (
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
   `dni` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `veterinario`
+--
+
+CREATE TABLE `veterinario` (
+  `id_veterinario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -67,7 +83,9 @@ CREATE TABLE `usuarios` (
 -- Indices de la tabla `animales`
 --
 ALTER TABLE `animales`
-  ADD PRIMARY KEY (`id_animales`);
+  ADD PRIMARY KEY (`id_animales`),
+  ADD KEY `fk_animales_veterinario` (`id_veterinario`),
+  ADD KEY `fk_animales_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `perfiles`
@@ -79,7 +97,13 @@ ALTER TABLE `perfiles`
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `veterinario`
+--
+ALTER TABLE `veterinario`
+  ADD PRIMARY KEY (`id_veterinario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -101,7 +125,24 @@ ALTER TABLE `perfiles`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `veterinario`
+--
+ALTER TABLE `veterinario`
+  MODIFY `id_veterinario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `animales`
+--
+ALTER TABLE `animales`
+  ADD CONSTRAINT `fk_animales_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_animales_veterinario` FOREIGN KEY (`id_veterinario`) REFERENCES `veterinario` (`id_veterinario`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
